b5594c7de8406685717bd343cd0b2f186134fa55
Serhii_Kononenko <sergeykono@gmail.com>
2018-04-12 14:49:38 +0300

    anchors added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,17 +1,42 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import {TranslateService} from '@ngx-translate/core';
+import { Router, NavigationEnd } from '@angular/router';
+import { LocationStrategy } from '@angular/common';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  constructor(private translate: TranslateService) {
+export class AppComponent implements OnInit {
+  isPopState = false;
+
+  constructor(private translate: TranslateService, private router: Router, private locStrat: LocationStrategy) {
     translate.addLangs(["en", "uk", "ru"]);
     translate.setDefaultLang('en');
 
     let browserLang = translate.getBrowserLang();
     translate.use(browserLang.match(/en|uk|ru/) ? browserLang : 'en');
   }
+
+
+  ngOnInit(): void {
+    this.locStrat.onPopState(() => {
+      this.isPopState = true;
+    });
+
+    this.router.events.subscribe(event => {
+      // Scroll to top if accessing a page, not via browser history stack
+      if (event instanceof NavigationEnd && !this.isPopState) {
+        window.scrollTo(0, 0);
+        this.isPopState = false;
+      }
+
+      // Ensures that isPopState is reset
+      if (event instanceof NavigationEnd) {
+        this.isPopState = false;
+      }
+    });
+  }
+
 }
